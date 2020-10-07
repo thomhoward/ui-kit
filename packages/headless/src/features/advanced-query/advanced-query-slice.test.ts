4ca138a24ce5d136c5fcc8867b55ed53b4833229
Jean-Luc Blais-Amyot <jlblais@coveo.com>
2020-10-07 10:53:19 -0400

    KIT-183 history change case
    https://coveord.atlassian.net/browse/KIT-183

--- packages/headless/src/features/advanced-query/advanced-query-slice.test.ts
+++ packages/headless/src/features/advanced-query/advanced-query-slice.test.ts
@@ -4,6 +4,8 @@ import {
 } from './advanced-query-slice';
 import {AdvancedQueryState} from '../../state';
 import {updateAdvancedQuery} from './advanced-query-actions';
+import {getHistoryEmptyState} from '../history/history-slice';
+import {change} from '../history/history-actions';
 
 describe('Advanced query slice', () => {
   const aq = 'mock-aq';
@@ -17,5 +19,21 @@ describe('Advanced query slice', () => {
     const action = updateAdvancedQuery(aq);
     state = advancedQueryReducer(state, action);
     expect(state.aq).toBe(aq);
+  });
+
+  it('allows a restore query on history change', () => {
+    const state = getInitialAdvancedQueryState();
+    const expectedQuery: AdvancedQueryState = {
+      aq: 'mock-aq',
+    };
+    const historyChange = {
+      ...getHistoryEmptyState(),
+      advancedQuery: expectedQuery,
+    };
+    const nextState = advancedQueryReducer(
+      state,
+      change.fulfilled(historyChange, '')
+    );
+    expect(nextState).toEqual(expectedQuery);
   });
 });
--- packages/headless/src/features/advanced-query/advanced-query-slice.ts
+++ packages/headless/src/features/advanced-query/advanced-query-slice.ts
@@ -1,6 +1,7 @@
 import {AdvancedQueryState} from '../../state';
 import {createReducer} from '@reduxjs/toolkit';
 import {updateAdvancedQuery} from './advanced-query-actions';
+import {change} from '../history/history-actions';
 
 export const getInitialAdvancedQueryState: () => AdvancedQueryState = () => ({
   aq: '',
@@ -9,8 +10,10 @@ export const getInitialAdvancedQueryState: () => AdvancedQueryState = () => ({
 export const advancedQueryReducer = createReducer(
   getInitialAdvancedQueryState(),
   (builder) => {
-    builder.addCase(updateAdvancedQuery, (state, action) => {
-      state.aq = action.payload;
-    });
+    builder
+      .addCase(updateAdvancedQuery, (state, action) => {
+        state.aq = action.payload;
+      })
+      .addCase(change.fulfilled, (_, action) => action.payload.advancedQuery);
   }
 );
