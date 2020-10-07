e52948c353ad2cfa88bce6ba4f275bc4932d8617
Jean-Luc Blais-Amyot <jlblais@coveo.com>
2020-10-07 18:00:16 -0400

    KIT-184 added change case
    https://coveord.atlassian.net/browse/KIT-184

--- packages/headless/src/features/visitorId/visitorId-slice.ts
+++ packages/headless/src/features/visitorId/visitorId-slice.ts
@@ -1,5 +1,6 @@
 import {createReducer} from '@reduxjs/toolkit';
 import {updateVisitorId} from './visitorId-actions';
+import {change} from '../history/history-actions';
 
 export type VisitorId = string;
 
@@ -8,6 +9,8 @@ export const getInitialVisitorIdState: () => VisitorId = () => '';
 export const visitorIdReducer = createReducer(
   getInitialVisitorIdState(),
   (builder) => {
-    builder.addCase(updateVisitorId, (_, action) => action.payload);
+    builder
+      .addCase(updateVisitorId, (_, action) => action.payload)
+      .addCase(change.fulfilled, (_, action) => action.payload.visitorId);
   }
 );
