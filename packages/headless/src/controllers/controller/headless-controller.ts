a010e13c809a4266fa5511a086025717012a5d59
Nafees Rawji <nrawji@coveo.com>
2020-10-07 11:43:50 -0400

    perf(headless): reduce unccessary calls to subscribe listener in controllers
    KIT-187
    
    https://coveord.atlassian.net/browse/KIT-187

--- packages/headless/src/controllers/controller/headless-controller.ts
+++ packages/headless/src/controllers/controller/headless-controller.ts
@@ -3,18 +3,37 @@ import {Engine} from '../../app/headless-engine';
 export type Controller = ReturnType<typeof buildController>;
 
 export function buildController(engine: Engine) {
-  const subscribe = (listener: () => void) => {
-    listener();
-    return engine.subscribe(() => listener());
-  };
-
   return {
+    prevState: '',
+
+    /**
+     * Determines whether or not the state has changed between two subscribe calls
+     * @returns A boolean representing whether the state has changed
+     */
+    hasStateChanged(): boolean {
+      const currentState = JSON.stringify(this.state);
+      const prevState = this.prevState;
+      this.prevState = currentState;
+      return prevState !== currentState;
+    },
+
     /**
      * Adds a callback that will be called on state change.
      *
      * @param listener A callback to be invoked on state change.
      * @returns An unsubscribe function to remove the listener.
      */
-    subscribe,
+    subscribe(listener: () => void) {
+      listener();
+      return engine.subscribe(() => {
+        if (this.hasStateChanged()) {
+          listener();
+        }
+      });
+    },
+
+    get state() {
+      return {};
+    },
   };
 }
