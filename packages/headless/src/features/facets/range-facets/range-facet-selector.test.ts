10fe3b7c1f0ff6e7760ae04f3acd7d318e2de53c
Nafees Rawji <nrawji@coveo.com>
2020-10-19 13:32:54 -0400

    feat(headless): add support for breadcrumbs
    KIT-96
    
    https://coveord.atlassian.net/browse/KIT-96

---
+++ packages/headless/src/features/facets/range-facets/range-facet-selector.test.ts
@@ -0,0 +1,10 @@
+import {SearchPageState} from '../../../state';
+import {createMockState} from '../../../test';
+
+describe('range facet selectors', () => {
+  let state: SearchPageState;
+
+  beforeEach(() => {
+    state = createMockState();
+  });
+});
