21e451f4ce7540c816a25dcc09b9d7a658ce2bad
Jean-Luc Blais-Amyot <jlblais@coveo.com>
2020-10-21 10:47:38 -0400

    KIT-87 removed class and export function
    https://coveord.atlassian.net/browse/KIT-87

--- packages/headless/src/utils/highlight.test.ts
+++ packages/headless/src/utils/highlight.test.ts
@@ -1,4 +1,4 @@
-import {HighlightKeyword, HighlightUtils} from './highlight';
+import {HighlightKeyword, highlightString} from './highlight';
 
 describe('highlight', () => {
   const lorem =
@@ -13,21 +13,21 @@ describe('highlight', () => {
   it('should wrap the passed highlights with tags using the specified class name', () => {
     const expectedHighlight =
       'Lor<span>em ip</span>su<span>m do</span>lor <span>sit amet, conse</span>ctetur adipi<span>sicing eli</span>t, sed do eiusmod tempor incididunt ut';
-    expect(
-      HighlightUtils.highlightString(lorem, highlights, '<span>', '</span>')
-    ).toBe(expectedHighlight);
+    expect(highlightString(lorem, highlights, '<span>', '</span>')).toBe(
+      expectedHighlight
+    );
   });
 
   it('should return the string unchanged if "content" is an empty string', () => {
     const expectedString = '';
-    expect(
-      HighlightUtils.highlightString('', highlights, '<span>', '</span>')
-    ).toBe(expectedString);
+    expect(highlightString('', highlights, '<span>', '</span>')).toBe(
+      expectedString
+    );
   });
 
   it('should throw if "tag" is an empty string', () => {
-    expect(() =>
-      HighlightUtils.highlightString(lorem, highlights, '', '</span>')
-    ).toThrow('delimiters should be a non-empty string');
+    expect(() => highlightString(lorem, highlights, '', '</span>')).toThrow(
+      'delimiters should be a non-empty string'
+    );
   });
 });
--- packages/headless/src/utils/highlight.ts
+++ packages/headless/src/utils/highlight.ts
@@ -16,48 +16,46 @@ function isEmptyString(str: string) {
   return str === '';
 }
 
-export class HighlightUtils {
-  /**
-   * Highlight the passed string using specified highlights.
-   * @param content The string to highlight items in.
-   * @param highlights The highlighted positions to highlight in the string.
-   * @param tag The opening tag to use when starting to highlight (e.g. <span class="my-class">).
-   */
-  static highlightString(
-    content: string,
-    highlights: HighlightKeyword[],
-    openingDelimiter: string,
-    closingDelimiter: string
-  ): string {
-    if (isEmptyString(openingDelimiter) || isEmptyString(closingDelimiter)) {
-      throw Error('delimiters should be a non-empty string');
-    }
-
-    if (isNullOrUndefined(content) || content === '') {
-      return content;
-    }
-    const maxIndex = content.length;
-    let highlighted = '';
-    let last = 0;
-    for (let i = 0; i < highlights.length; i++) {
-      const highlight = highlights[i];
-      const start: number = highlight.offset;
-      const end: number = start + highlight.length;
-
-      if (end > maxIndex) {
-        break;
-      }
-
-      highlighted += _.escape(content.slice(last, start));
-      highlighted += openingDelimiter;
-      highlighted += _.escape(content.slice(start, end));
-      highlighted += closingDelimiter;
-
-      last = end;
-    }
-    if (last !== maxIndex) {
-      highlighted += _.escape(content.slice(last));
-    }
-    return highlighted;
+/**
+ * Highlight the passed string using specified highlights.
+ * @param content The string to highlight items in.
+ * @param highlights The highlighted positions to highlight in the string.
+ * @param tag The opening tag to use when starting to highlight (e.g. <span class="my-class">).
+ */
+export function highlightString(
+  content: string,
+  highlights: HighlightKeyword[],
+  openingDelimiter: string,
+  closingDelimiter: string
+): string {
+  if (isEmptyString(openingDelimiter) || isEmptyString(closingDelimiter)) {
+    throw Error('delimiters should be a non-empty string');
   }
+
+  if (isNullOrUndefined(content) || content === '') {
+    return content;
+  }
+  const maxIndex = content.length;
+  let highlighted = '';
+  let last = 0;
+  for (let i = 0; i < highlights.length; i++) {
+    const highlight = highlights[i];
+    const start: number = highlight.offset;
+    const end: number = start + highlight.length;
+
+    if (end > maxIndex) {
+      break;
+    }
+
+    highlighted += _.escape(content.slice(last, start));
+    highlighted += openingDelimiter;
+    highlighted += _.escape(content.slice(start, end));
+    highlighted += closingDelimiter;
+
+    last = end;
+  }
+  if (last !== maxIndex) {
+    highlighted += _.escape(content.slice(last));
+  }
+  return highlighted;
 }
