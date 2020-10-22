9b0ac745efec18ce6a903a55a76b118bfc784675
Jean-Luc Blais-Amyot <jlblais@coveo.com>
2020-10-22 14:08:57 -0400

    KIT-87 no escaping & params object
    https://coveord.atlassian.net/browse/KIT-87

--- packages/headless/src/utils/highlight.ts
+++ packages/headless/src/utils/highlight.ts
@@ -11,48 +11,48 @@ export interface HighlightKeyword {
   length: number;
 }
 
+export interface HighlightParams {
+  /**
+   * The string to highlight items in.
+   */
+  content: string;
+  /**
+   * The highlighted positions to highlight in the string.
+   */
+  highlights: HighlightKeyword[];
+  /**
+   * The opening delimiter used when starting to highlight.
+   */
+  openingDelimiter: string;
+  /**
+   * The closing delimiter used to close highlighted section.
+   */
+  closingDelimiter: string;
+}
+
 function isEmptyString(str: string) {
   return str === '';
 }
 
 /**
- * Function to escape a string. For more information, refer to {@link https://underscorejs.org/#escape}
- */
-function escapeString(str: string): string {
-  return str
-    .replace('&', '&amp;')
-    .replace('<', '&lt;')
-    .replace('>', '&gt;')
-    .replace('"', '&quot;')
-    .replace("'", '&#x27;')
-    .replace('`', '&#96;');
-}
-
-/**
  * Highlight the passed string using specified highlights.
- * @param content The string to highlight items in.
- * @param highlights The highlighted positions to highlight in the string.
- * @param openingDelimiter The opening delimiter used when starting to highlight.
- * @param closingDelimiter The closing delimiter used to close highlighted section.
  */
-export function highlightString(
-  content: string,
-  highlights: HighlightKeyword[],
-  openingDelimiter: string,
-  closingDelimiter: string
-): string {
-  if (isEmptyString(openingDelimiter) || isEmptyString(closingDelimiter)) {
+export function highlightString(params: HighlightParams): string {
+  if (
+    isEmptyString(params.openingDelimiter) ||
+    isEmptyString(params.closingDelimiter)
+  ) {
     throw Error('delimiters should be a non-empty string');
   }
 
-  if (isNullOrUndefined(content) || isEmptyString(content)) {
-    return content;
+  if (isNullOrUndefined(params.content) || isEmptyString(params.content)) {
+    return params.content;
   }
-  const maxIndex = content.length;
+  const maxIndex = params.content.length;
   let highlighted = '';
   let last = 0;
-  for (let i = 0; i < highlights.length; i++) {
-    const highlight = highlights[i];
+  for (let i = 0; i < params.highlights.length; i++) {
+    const highlight = params.highlights[i];
     const start: number = highlight.offset;
     const end: number = start + highlight.length;
 
@@ -60,15 +60,15 @@ export function highlightString(
       break;
     }
 
-    highlighted += escapeString(content.slice(last, start));
-    highlighted += openingDelimiter;
-    highlighted += escapeString(content.slice(start, end));
-    highlighted += closingDelimiter;
+    highlighted += params.content.slice(last, start);
+    highlighted += params.openingDelimiter;
+    highlighted += params.content.slice(start, end);
+    highlighted += params.closingDelimiter;
 
     last = end;
   }
   if (last !== maxIndex) {
-    highlighted += escapeString(content.slice(last));
+    highlighted += params.content.slice(last);
   }
   return highlighted;
 }
