81111888cbb0540ed8930a11b36ca647abda2839
Jean-Luc Blais-Amyot <jlblais@coveo.com>
2020-11-03 16:00:29 -0500

    validate
    https://coveord.atlassian.net/browse/KIT-218

--- packages/headless/src/features/facets/range-facets/date-facet-set/date-facet-actions.ts
+++ packages/headless/src/features/facets/range-facets/date-facet-set/date-facet-actions.ts
@@ -3,13 +3,51 @@ import {DateFacetRegistrationOptions} from './interfaces/options';
 import {DateFacetValue} from './interfaces/response';
 import {updateRangeFacetSortCriterion} from '../generic/range-facet-actions';
 import {deselectAllFacetValues} from '../../facet-set/facet-set-actions';
+import {validatePayloadSchema} from '../../../../utils/validate-payload';
+import {NumberValue, BooleanValue, RecordValue, Value} from '@coveo/bueno';
+import {
+  facetIdDefinition,
+  requiredNonEmptyString,
+} from '../../generic/facet-actions-validation';
+import {RangeFacetSortCriterion} from '../generic/interfaces/request';
+import {DateRangeRequest} from './interfaces/request';
+// import { FacetValueState } from '../../facet-api/value';
+
+const dateFacetValueDefinition = {
+  state: requiredNonEmptyString,
+  numberOfResults: new NumberValue({required: true, min: 0}),
+  start: requiredNonEmptyString,
+  end: requiredNonEmptyString,
+  endInclusive: new BooleanValue({required: true}),
+};
+
+// const dateRangeRequestDefinition = {
+//   start: requiredNonEmptyString,
+//   end: requiredNonEmptyString,
+//   endInclusive: new BooleanValue({required:true}),
+//   state: new Value<FacetValueState>({required:true}),
+// }
+
+const dateFacetRegistrationOptionsDefinition = {
+  facetId: facetIdDefinition,
+  field: requiredNonEmptyString,
+  //currentValues:new ArrayValue({required:true, each:dateRangeRequestDefinition}),
+  currentValues: new Value<DateRangeRequest[]>({required: true}),
+  generateAutomaticRanges: new BooleanValue({required: true, default: true}),
+  filterFacetCount: new BooleanValue({required: false}),
+  injectionDepth: new NumberValue({required: false, min: 0}),
+  numberOfValues: new NumberValue({required: false, min: 1}),
+  sortCriteria: new Value<RangeFacetSortCriterion>({required: false}),
+};
 
 /**
  * Registers a date facet.
  * @param (DateFacetRegistrationOptions) The options to register the facet with.
  */
-export const registerDateFacet = createAction<DateFacetRegistrationOptions>(
-  'dateFacet/register'
+export const registerDateFacet = createAction(
+  'dateFacet/register',
+  (payload: DateFacetRegistrationOptions) =>
+    validatePayloadSchema(payload, dateFacetRegistrationOptionsDefinition)
 );
 
 /**
@@ -17,10 +55,14 @@ export const registerDateFacet = createAction<DateFacetRegistrationOptions>(
  * @param facetId (string) The unique identifier of the facet (e.g., `"1"`).
  * @param selection (DateFacetValue) The target date facet value.
  */
-export const toggleSelectDateFacetValue = createAction<{
-  facetId: string;
-  selection: DateFacetValue;
-}>('dateFacet/toggleSelectValue');
+export const toggleSelectDateFacetValue = createAction(
+  'dateFacet/toggleSelectValue',
+  (payload: {facetId: string; selection: DateFacetValue}) =>
+    validatePayloadSchema(payload, {
+      facetId: facetIdDefinition,
+      selection: new RecordValue(dateFacetValueDefinition),
+    })
+);
 
 /** Updates the sort criterion of a date facet.
  * @param facetId (string) The unique identifier of the facet (e.g., `"1"`).
--- packages/headless/src/features/facets/range-facets/numeric-facet-set/numeric-facet-actions.ts
+++ packages/headless/src/features/facets/range-facets/numeric-facet-set/numeric-facet-actions.ts
@@ -3,24 +3,57 @@ import {NumericFacetRegistrationOptions} from './interfaces/options';
 import {NumericFacetValue} from './interfaces/response';
 import {updateRangeFacetSortCriterion} from '../generic/range-facet-actions';
 import {deselectAllFacetValues} from '../../facet-set/facet-set-actions';
+import {validatePayloadSchema} from '../../../../utils/validate-payload';
+import {
+  facetIdDefinition,
+  requiredNonEmptyString,
+} from '../../generic/facet-actions-validation';
+import {RecordValue, NumberValue, BooleanValue, Value} from '@coveo/bueno';
+import {RangeFacetSortCriterion} from '../generic/interfaces/request';
+import {NumericRangeRequest} from './interfaces/request';
+
+const numericFacetValueDefinition = {
+  state: requiredNonEmptyString,
+  numberOfResults: new NumberValue({required: true, min: 0}),
+  start: new NumberValue({required: true}),
+  end: new NumberValue({required: true}),
+  endInclusive: new BooleanValue({required: true}),
+};
+
+const numericFacetRegistrationOptionsDefinition = {
+  facetId: facetIdDefinition,
+  field: requiredNonEmptyString,
+  currentValues: new Value<NumericRangeRequest[]>({required: true}),
+  generateAutomaticRanges: new BooleanValue({required: true}),
+  filterFacetCount: new BooleanValue({required: false}),
+  injectionDepth: new NumberValue({required: false, min: 0}),
+  numberOfValues: new NumberValue({required: false, min: 1}),
+  sortCriteria: new Value<RangeFacetSortCriterion>({required: false}),
+};
 
 /**
  * Registers a numeric facet.
  * @param (NumericFacetRegistrationOptions) The options to register the facet with.
  */
-export const registerNumericFacet = createAction<
-  NumericFacetRegistrationOptions
->('numericFacet/register');
+export const registerNumericFacet = createAction(
+  'numericFacet/register',
+  (payload: NumericFacetRegistrationOptions) =>
+    validatePayloadSchema(payload, numericFacetRegistrationOptionsDefinition)
+);
 
 /**
  * Toggles a numeric facet value.
  * @param facetId (string) The unique identifier of the facet (e.g., `"1"`).
  * @param selection (NumericFacetValue) The target numeric facet value.
  */
-export const toggleSelectNumericFacetValue = createAction<{
-  facetId: string;
-  selection: NumericFacetValue;
-}>('numericFacet/toggleSelectValue');
+export const toggleSelectNumericFacetValue = createAction(
+  'numericFacet/toggleSelectValue',
+  (payload: {facetId: string; selection: NumericFacetValue}) =>
+    validatePayloadSchema(payload, {
+      facetId: facetIdDefinition,
+      selection: new RecordValue(numericFacetValueDefinition),
+    })
+);
 
 /** Updates the sort criterion of a numeric facet.
  * @param facetId (string) The unique identifier of the facet (e.g., `"1"`).
