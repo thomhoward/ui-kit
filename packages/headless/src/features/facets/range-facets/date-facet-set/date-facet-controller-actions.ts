612d75a5936ad553e7516e50ac7229bbb518a723
Jean-Luc Blais-Amyot <jlblais@coveo.com>
2020-11-12 13:54:45 +0000

    feat(headless):  range facets validation https://coveord.atlassian.net/browse/KIT-218
    Approved-by: Jean-Felix Thibodeau
    Approved-by: Olivier Lamothe
    Approved-by: Sami Sayegh

--- packages/headless/src/features/facets/range-facets/date-facet-set/date-facet-controller-actions.ts
+++ packages/headless/src/features/facets/range-facets/date-facet-set/date-facet-controller-actions.ts
@@ -7,6 +7,20 @@ import {
 } from '../../../../state/state-sections';
 import {executeToggleRangeFacetSelect} from '../generic/range-facet-controller-actions';
 import {toggleSelectDateFacetValue} from './date-facet-actions';
+import {validatePayloadSchema} from '../../../../utils/validate-payload';
+import {
+  requiredNonEmptyString,
+  facetIdDefinition,
+} from '../../generic/facet-actions-validation';
+import {BooleanValue, NumberValue, RecordValue} from '@coveo/bueno';
+
+const dateFacetValueDefinition = {
+  start: requiredNonEmptyString,
+  end: requiredNonEmptyString,
+  endInclusive: new BooleanValue({required: true}),
+  state: requiredNonEmptyString,
+  numberOfResults: new NumberValue({required: true, min: 0}),
+};
 
 /**
  * Toggles the date facet value and then executes a search with the appropriate analytics tag.
@@ -21,6 +35,13 @@ export const executeToggleDateFacetSelect = createAsyncThunk<
   },
   AsyncThunkSearchOptions<ConfigurationSection & DateFacetSection>
 >('dateFacet/executeToggleSelect', ({facetId, selection}, {dispatch}) => {
+  validatePayloadSchema(
+    {facetId, selection},
+    {
+      facetId: facetIdDefinition,
+      selection: new RecordValue({values: dateFacetValueDefinition}),
+    }
+  );
   dispatch(toggleSelectDateFacetValue({facetId, selection}));
   dispatch(executeToggleRangeFacetSelect({facetId, selection}));
 });
